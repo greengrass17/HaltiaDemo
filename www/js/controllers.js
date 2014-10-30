af5640a6e8d68114b6fb7cb3d32ad234001d0bf0
Vu Dao <anhvudao17@gmail.com>
2014-10-30 03:00:26 +0200

    Change images to real photos

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -49,12 +49,21 @@ angular.module('starter.controllers', [])
 
 .controller('GalleryCtrl', function($scope) {
   $scope.photos = [
-    { link: "img/img (1).png", id: 1 },
-    { link: "img/img (2).png", id: 2 },
-    { link: "img/img (3).png", id: 3 },
-    { link: "img/img (4).png", id: 4 },
-    { link: "img/img (5).png", id: 5 },
-    { link: "img/img (6).png", id: 6 },
+    { link: "img/img (1).jpg", id: 1 },
+    { link: "img/img (2).jpg", id: 2 },
+    { link: "img/img (3).jpg", id: 3 },
+    { link: "img/img (4).jpg", id: 4 },
+    { link: "img/img (5).jpg", id: 5 },
+    { link: "img/img (6).jpg", id: 6 },
+    { link: "img/img (7).jpg", id: 7 },
+    { link: "img/img (8).jpg", id: 8 },
+    { link: "img/img (9).jpg", id: 9 },
+    { link: "img/img (10).jpg", id: 10 },
+    { link: "img/img (11).jpg", id: 11 },
+    { link: "img/img (12).jpg", id: 12 },
+    { link: "img/img (13).jpg", id: 13 },
+    { link: "img/img (14).jpg", id: 14 },
+    { link: "img/img (15).jpg", id: 15 },
   ];
 
   // initial image index
